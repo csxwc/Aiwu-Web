b095a1ad05692365125a4365d05fc3621f6143f7
wang <1393065872@qq.com>
2019-03-07 12:51:25 +0800

    收藏按钮

--- src/components/HouseInfo.vue
+++ src/components/HouseInfo.vue
@@ -3,7 +3,6 @@
     <v-header></v-header>
     <v-carouse></v-carouse>
     <br>
-    <br>
     <div style="width: 80%; margin: 0 auto">
       <Row :gutter="30">
         <Col span="2">
@@ -16,7 +15,10 @@
         <Col span="15">
           <div id="detail">
             {{this.house.city}}·{{this.house.type}}
-            <div><strong style="font-size: 25px;">{{this.house.name}}</strong></div>
+            <div>
+              <strong style="font-size: 25px;">{{this.house.name}}</strong>
+              <Button type="primary" icon="md-add" style="float: right;">点击收藏</Button>
+            </div>
             <hr>
             <ul class="info">
               <li>
@@ -36,7 +38,6 @@
                 最多容纳{{this.house.guest}}个人
               </li>
             </ul>
-            <br>
             <br>
             <!--<Input :rows="20" readonly style="font-size: 18px;" type="textarea" v-model="this.house.introduction"/>-->
             <div style="white-space: pre-wrap; font-size: 18px">
@@ -98,6 +99,7 @@
 <script>
   import Header from '../components/Header'
   import VCarouse from "./home_components/Carousel";
+  import Check from "../model/Check";
 
   export default {
     name: "HouseInfo",
@@ -107,7 +109,8 @@
         position:{
           lng:'',
           lat:''
-        }
+        },
+        btnPrefrer:'点击收藏'
       }
     },
     components: {
@@ -122,14 +125,19 @@
       this.$axios
         .post('http://localhost:8888/house/getallinfo', {houseid:parseInt(this.$route.params.houseid)})
         .then(response => {
-          console.log(response);
+          // console.log(response);
           this.house=response.data;
           this.position.lat = response.data.jingdu;
           this.position.lng = response.data.weidu;
         })
         .catch(error => {
           console.log(error)
-        })
+        });
+
+      if(Check.isLogged()){
+
+      }
+
     }
   }
 </script>
