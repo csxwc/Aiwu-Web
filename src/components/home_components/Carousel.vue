9688d1d753a060393895aca3788f468cf2fcffa3
wang <1393065872@qq.com>
2019-03-08 17:54:57 +0800

    轮播图图片更换

--- src/components/home_components/Carousel.vue
+++ src/components/home_components/Carousel.vue
@@ -7,16 +7,13 @@
     :trigger="'hover'"
     :arrow="'hover'">
     <CarouselItem>
-      <div class="imgc"><img src="http://cyldurl.roogames.com/uploadfile/2015/0129/20150129083714643.jpg" alt=""></div>
+      <div class="imgc"><img src="https://z1.muscache.cn/pictures/00b12c64-0851-40e7-83aa-6bba10221435.jpg" alt=""></div>
     </CarouselItem>
     <CarouselItem>
-      <div class="imgc"><img src="http://cyldurl.roogames.com/uploadfile/2015/0129/20150129083717796.jpg" alt=""></div>
+      <div class="imgc"><img src="https://z1.muscache.cn/pictures/9bd8f08c-2e78-4665-ab7b-e4952673dabd.jpg" alt=""></div>
     </CarouselItem>
     <CarouselItem>
-      <div class="imgc"><img src="http://cyldurl.roogames.com/uploadfile/2015/0106/20150106085726706.jpg" alt=""></div>
-    </CarouselItem>
-    <CarouselItem>
-      <div class="imgc"><img src="http://cyldurl.roogames.com/uploadfile/2015/0311/20150311082948747.jpg" alt=""></div>
+      <div class="imgc"><img src="https://z1.muscache.cn/pictures/30c10f87-af4b-45e5-8b6d-1a95c88fd5dd.jpg" alt=""></div>
     </CarouselItem>
   </Carousel>
 </template>
@@ -29,5 +26,9 @@
 <style>
   .imgc{
     height: 500px;
+    width: 100%;
+  }
+  .imgc img{
+    width: 100%;
   }
 </style>
