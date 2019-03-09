1c00c9c0196f6c0a0db6866e74fb43a83f7fc38f
wang <1393065872@qq.com>
2019-03-09 10:00:35 +0800

    轮播图图片更换

--- src/components/HouseInfo.vue
+++ src/components/HouseInfo.vue
@@ -17,8 +17,8 @@
             {{this.house.city}}·{{this.house.type}}
             <div>
               <strong style="font-size: 25px;">{{this.house.name}}</strong>
-              <Button icon="md-add" style="float: right;" @click="cancel"  type="primary" v-if="preferred">取消收藏</Button>
-              <Button icon="md-add" style="float: right;"  type="primary" @click="prefer" v-else>点击收藏</Button>
+              <Button @click="cancel" icon="md-add" style="float: right;" type="primary" v-if="preferred">取消收藏</Button>
+              <Button @click="prefer" icon="md-add" style="float: right;" type="primary" v-else>点击收藏</Button>
             </div>
             <hr>
             <ul class="info">
@@ -78,8 +78,14 @@
               <Card :bordered="false" style="width: 100%">
                 <p slot="title">¥{{this.house.price}}每晚</p>
                 <p>日期:</p>
-                <DatePicker confirm placeholder="Select date" placement="bottom-end" style="width: 200px"
-                            type="daterange"></DatePicker>
+                <DatePicker confirm placeholder="Select date"
+                            placement="bottom-end"
+                            style="width: 200px"
+                            type="daterange"
+                            v-model="selecteddate"
+                            format="yyyy-MM-dd">
+
+                </DatePicker>
                 <br>
                 <br>
                 <p>人数:</p>
@@ -88,7 +94,7 @@
                 <br>
                 <br>
                 <br>
-                <Button :type="'primary'" style="width: 80%">预定</Button>
+                <Button :type="'primary'" style="width: 80%" @click="test">预定</Button>
               </Card>
             </div>
 
@@ -118,6 +124,8 @@
           lat: ''
         },
         preferred: false,
+
+        selecteddate:null,
       }
     },
     components: {
@@ -125,51 +133,57 @@
       'v-header': Header
     },
     methods: {
-      cancel(){
+      book(){
+        this.$axios.post("http://localhost:8888/")
+      },
+
+      cancel() {
         // console.log("是否收藏"+this.preferred);
 
-        this.$axios.get("http://localhost:8888/collection/"+GetInfo.getUserIDByLocalStorage())
-          .then(resp=> {
+        this.$axios.get("http://localhost:8888/collection/" + GetInfo.getUserIDByLocalStorage())
+          .then(resp => {
             // console.log(length);
-            for(var i = 0; i < resp.data.length;i++){
-              if(parseInt(this.$route.params.houseid) === parseInt(resp.data[i].room_id)){
-                this.$axios.delete("http://localhost:8888/collection/delete/"+resp.data[i].id);
+            for (var i = 0; i < resp.data.length; i++) {
+              if (parseInt(this.$route.params.houseid) === parseInt(resp.data[i].room_id)) {
+                this.$axios.delete("http://localhost:8888/collection/delete/" + resp.data[i].id);
               }
             }
 
           })
-          .catch(error=>{
+          .catch(error => {
             console.log(error)
           });
 
-        this.preferred=false;
+        this.preferred = false;
         this.$Message.success('取消收藏成功');
 
       },
-      prefer(){
-        this.$axios
-          .post("http://localhost:8888/collection/add",
-            {user_id: parseInt(GetInfo.getUserIDByLocalStorage()),house_id:parseInt(this.$route.params.houseid)})
-          .then(resp=>{
-            console.log(resp);
-          })
-          .catch(error=>{
-            console.log(error);
-          });
-        this.preferred = true;
-        this.$Message.success('已添加收藏');
+      prefer() {
+        if (Check.isLogged()) {
+          this.$axios
+            .post("http://localhost:8888/collection/add",
+              {user_id: parseInt(GetInfo.getUserIDByLocalStorage()), house_id: parseInt(this.$route.params.houseid)})
+            .then(resp => {
+              console.log(resp);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+          this.preferred = true;
+          this.$Message.success('已添加收藏');
+        } else {
+          this.$Message.warning('您没有登录，请登录');
+        }
       }
 
     },
-    computed: {
-
-    },
+    computed: {},
     mounted() {
       this.$axios
         .post('http://localhost:8888/house/getallinfo',
           {houseid: parseInt(this.$route.params.houseid)})
         .then(response => {
-          // console.log(response);
+          console.log(response);
           this.house = response.data;
           this.position.lat = response.data.jingdu;
           this.position.lng = response.data.weidu;
@@ -178,20 +192,21 @@
           console.log(error)
         });
 
-      if(Check.isLogged()){
+      if (Check.isLogged()) {
         this.$axios
           .post("http://localhost:8888/collection/iscollected",
-            {user_id:parseInt(localStorage.getItem('userid')),
-              house_id:parseInt(this.$route.params.houseid)})
-          .then(response=>{
+            {
+              user_id: parseInt(localStorage.getItem('userid')),
+              house_id: parseInt(this.$route.params.houseid)
+            })
+          .then(response => {
             // console.log(response.data);
             this.preferred = response.data;
           })
-          .catch(error=>{
+          .catch(error => {
             console.log(error)
-          })
-      }
-      else
+          });
+      } else
         this.preferred = false;
     }
   }
